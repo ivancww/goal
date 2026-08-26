de24ff2cf01eebbf165b4ddf0b6d8e5b2935610f
ivancww <wahchiu0903@gmail.com>
2026-08-26 11:12:37 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'ivan-performance-os-v3.4.0';
-const APP_VERSION = 'v3.4.0';
+const CACHE_NAME = 'ivan-performance-os-v3.4.1';
+const APP_VERSION = 'v3.4.1';
 
 const STATIC_ASSETS = [
   './',
